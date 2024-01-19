0d716835688c590129f9b5655b04950267876f71
momoasr <monia_akele@yahoo.fr>
2024-01-19 16:15:47 -0500

    connect to js

--- test.js
+++ test.js
@@ -1,28 +1,12 @@
-const allOrders = [
-    {
-        title: "la vie est belle",
-        author: "Papa Wemba",
-        genre: "Movie",
-        isGood: true
-    }, 
-    {
-        title: "Plus Belle la vie",
-        author: "Canal France",
-        genre: "Series",
-        isGood: true 
-    },
-    {
-        title: "Elle n est pas belle la vie?",
-        author: "Monia",
-        genre: "Thought",
-        isGood: false 
-    }
-];
 
-const goodOrders = allOrders.filter(arrItem => {
-    return arrItem.isGood;
-})
+const initSlider = () => {
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slider-button");
 
-function searchButton(){
-    alert('someone search something');
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            console.log();
+        });
+    })
 }
+
+window.addEventListener("load", initSlider);
