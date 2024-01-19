6ae95fe57bbdbed14901ae456a9f01106247c09c
momoasr <monia_akele@yahoo.fr>
2024-01-19 16:42:32 -0500

    slide button function in js

--- test.js
+++ test.js
@@ -1,12 +1,27 @@
 
 const initSlider = () => {
-    const slideButtons = document.querySelectorAll(".slider-wrapper .slider-button");
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
 
+    //slide image according to the slide button clicks
     slideButtons.forEach(button => {
         button.addEventListener("click", () => {
-            console.log();
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" })
         });
-    })
+    });
+
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "block";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+    }
+
+    imageList.addEventListener("scroll", () => {
+        handleSlideButtons();
+    });
+
 }
 
 window.addEventListener("load", initSlider);
